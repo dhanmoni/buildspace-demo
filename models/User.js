f7b0cd82184ac51a275c991d184cad2586de888c
Dhanmoni Nath <dhanmoninath989@gmail.com>
2022-05-22 15:09:41 +0530

    deployment ready

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema(
+  {
+    publicKey:{
+        type: String,
+    },
+    nonce: {
+        type: Number,
+        default: () => Math.floor(Math.random() * 1000000) // Initialize with a random nonce
+    },
+    name: {
+        type: String,
+    },
+    email:{
+        type: String,
+    },
+    bio: {
+        type: String
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = User = mongoose.model("user", schema);
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv').config();
+const router = require('./router')
+const path = require('path')
+
+const app = express()
+
+//body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// setting up cors, headers
+app.use(cors());
+
+//connect to mongodb
+mongoose.connect(process.env.MONGODB_ATLAS_URI,
+    err => {
+        if(err) throw err;
+        console.log('connected to MongoDB')
+    }
+);
+// For production
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.resolve(__dirname, "./client/build")));
+
+  app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  });
+}
+
+
+// port
+const port = process.env.PORT || 5000;
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
