8f150281d5338ea4e4c22f8f32ac60a40d5039f3
Dhanmoni Nath <dhanmoninath989@gmail.com>
2022-05-26 20:05:53 +0530

    bug fixed

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ mongoose.connect(process.env.MONGODB_ATLAS_URI,
 );
 // For production
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.resolve(__dirname, "./client/build")));
+  app.use(express.static(path.resolve(__dirname, "./client/dist")));
 
   app.get("*", function (request, response) {
-    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+    response.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
   });
 }
 
