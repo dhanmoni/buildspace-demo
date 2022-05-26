babc3a92e4adb5800afcb6dc5c271ea08e67e0c1
Dhanmoni Nath <dhanmoninath989@gmail.com>
2022-05-26 20:53:39 +0530

    bug fixed

--- client/src/util/api.js
+++ client/src/util/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://buildspace-demo.herokuapp.com",
   headers: {
     "Content-Type": "application/json",
   },
