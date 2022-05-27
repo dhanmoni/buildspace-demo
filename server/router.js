fb6cd2738e4ded26fcd8eba84c1c185044af0835
Dhanmoni Nath <dhanmoninath989@gmail.com>
2022-05-27 13:42:52 +0530

    ready for deployment

--- server/router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('./routes/api/auth')
-const profile = require('./routes/api/profile')
-
-router.use('/api/auth', auth)
-router.use('/api/profile', profile)
-
-module.exports = router;
--- server/routes/api/profile.js
+++
@@ -1,9 +0,0 @@
-
-const express = require("express");
-const router = express.Router();
-const auth = require('../../middleware/authMiddleware')
-const profileController = require("../../controller/profileController");
-
-router.post("/create-profile",auth, profileController.createUserProfile);
-
-module.exports = router;
