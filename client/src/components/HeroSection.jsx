33a3d3759efbad04e666cdaa19e90a8aad1ca4cb
Dhanmoni Nath <dhanmoninath989@gmail.com>
2022-06-06 21:10:18 +0530

    minor changes, responsiveness added

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import '../styles/HeroSection.css'
+import { Link } from "react-router-dom";
+import "../styles/HeroSection.css";
 
 function HeroSection() {
   return (
     <div id="hero-section">
-      <div id="title">Home to the <br/> <span>world's best</span> web3 <br/> builders.</div>
+      <div id="title">
+        Home to the <br /> <span>world's best</span> web3 <br /> builders.
+      </div>
       <div id="sub-title">
         Buildspace accelerates your builder journey into web3. Whether you're
         just starting out, a seasoned vet transitioning from web2, or thinking
@@ -12,8 +15,18 @@ function HeroSection() {
         incredible builders doing just that.
       </div>
       <div id="action-btns">
-        <div id="btn-project">Start a Project</div>
-        <div id="btn-discord">Join Discord</div>
+        <div id="btn-project">
+          <Link to="/login" style={{textDecoration:'none', color: '#333'}}>
+            Start a Project
+          </Link>
+        </div>
+        <div id="btn-discord">
+          <Link to="/login" style={{textDecoration:'none', color: '#fff'}}>
+            Join Discord
+          </Link>
+        </div>
+        {/* <div id="btn-project">Start a Project</div>
+        <div id="btn-discord">Join Discord</div> */}
       </div>
     </div>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from "react-router-dom";
+import '../styles/Home.css'
 
 function Home() {
   const navigate = useNavigate()
@@ -14,10 +15,13 @@ function Home() {
       navigate('/')
     }
   return (
-    <div>
-      <h5>Succesfully Signed In using Metamask! </h5>
-      <h6>JWT Token: {token}</h6>
-      <button onClick={handleSignout} >Sign Out</button>
+    <div style={{width: '80vw', margin: '0 auto'}}>
+      <h5>Woohooo!!! Succesfully Signed In using Metamask!</h5>
+      <h6>Here is your JWT Token: {token}</h6>
+      <h6>Ready to try it again? Sign out by clicking the button below</h6>
+      <div className='btn-container'>
+        <button onClick={handleSignout}>Sign Out</button>
+      </div>
       </div>
   )
 }
